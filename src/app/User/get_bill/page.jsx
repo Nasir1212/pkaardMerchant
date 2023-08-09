58fce0fcad82534ffab0e16bb3b24064f0a78d9e
Nasir1212 <nnasiruddin1996@gmail.com>
2023-08-09 17:30:25 +0600

    9 aug 23

--- src/app/User/get_bill/page.jsx
+++ src/app/User/get_bill/page.jsx
@@ -2,9 +2,59 @@
 import { useState,useEffect } from "react";
 import { useSelector } from "react-redux";
 import http from "../../../../Component/http";
+import { useRouter } from 'next/navigation'
+import { useSearchParams } from 'next/navigation';
 
   function  NewOrder() {
+   const [orderData,setOrderData] = useState([]);
+   const [Calculator,setCalculator] = useState({myDiscount:null,myPayablePrice:null,grandTotal:null,Vat:null,Charge:null});
+const router = useRouter()
    
+const useSearch =useSearchParams();
+let order_id = useSearch.get("order_id");
+
+useEffect( ()=>{
+    getData();
+ },[])
+
+ function getData() {
+
+ http.get(`order_card_holder_by_tid/${order_id}`)
+ .then(data=>{
+     if(data.status==200){
+         setOrderData(data.data);
+
+     }
+    
+ })
+
+}
+
+function handleChange(e){
+   let {aff_sub_discount_product_privilege,affiliation_product_privilege} = orderData[0];
+let value = e.target.value
+console.log(value)
+
+let privilege = aff_sub_discount_product_privilege == null ?affiliation_product_privilege:aff_sub_discount_product_privilege;
+
+console.log(privilege);
+
+if(Number(privilege) != NaN){
+
+    let discount = value * Number(privilege) /100;
+    let payable_price = value - discount;
+    let charge = payable_price * 1 /100;
+    let vat = charge * 15 /100;
+
+    let grand_total = charge + vat;
+
+    setCalculator(value=>({...value,myDiscount:discount,myPayablePrice:payable_price,grandTotal:grand_total,Vat:vat,Charge:charge}))
+}
+
+
+}
+console.log(Calculator)
+
     return ( <>
     
     <main >
@@ -19,28 +69,49 @@ import http from "../../../../Component/http";
 
             <tbody>
                <tr>
-                <td>Coupon no</td>
-                <td><input type="text" className="form-control" style={{background:"rgb(241 98 45 / 56%)",fontWeight:"bold"}} name="" id="" /></td>
+                <td>Name</td>
+                <td><input type="text" disabled value={ orderData.length != 0? orderData[0]['full_name']:''} className="form-control" style={{background:"rgb(241 98 45 / 56%)",fontWeight:"bold"}} name="" id="" /></td>
                 </tr>
 
-                 <tr>
+                <tr>
                 <td>Card no</td>
-                <td><input type="text"  className="form-control" style={{background:"rgb(241 98 45 / 56%)",fontWeight:"bold"}} name="" id="" /></td>
-                </tr>        
+                <td><input type="text" disabled  value={ orderData.length != 0? orderData[0]['card_no']:''}  className="form-control" style={{background:"rgb(241 98 45 / 56%)",fontWeight:"bold"}} name="" id="" /></td>
+                </tr>    
 
                 <tr>
-                <td>Bill </td>
-                <td><input type="text"  className="form-control" style={{background:"rgb(241 98 45 / 56%)",fontWeight:"bold"}} name="" id="" /></td>
+                <td>Coupon no</td>
+                <td><input type="text" disabled value={  orderData.length != 0?  orderData[0]['discount_promo_code']:''} className="form-control" style={{background:"rgb(241 98 45 / 56%)",fontWeight:"bold"}} name="" id="" /></td>
                 </tr>
 
-                <tr>
-                <td>Discount</td>
-                <td><input type="text"  className="form-control" style={{background:"rgb(241 98 45 / 56%)",fontWeight:"bold"}} name="" id="" /></td>
+                  
+
+                <tr style={orderData.length != 0 && orderData[0]['payable_price'] == null ? {display:""}:{display:"none"}}>
+                <td>Bill </td>
+                <td><input onChange={(e)=>handleChange(e)}   type="text"    className="form-control" style={{background:"rgb(241 98 45 / 56%)",fontWeight:"bold"}} name="" id="" /></td>
+                </tr>
+
+                <tr  style={orderData.length != 0 && orderData[0]['payable_price'] == null ? {display:""}:{display:"none"}}>
+                <td >Discount</td>
+                <td><input type="text" disabled value={Calculator['myDiscount']}  className="form-control" style={{background:"rgb(241 98 45 / 56%)",fontWeight:"bold"}} name="" id="" /></td>
                 </tr>
 
                 <tr>
                 <td>Payable Bill </td>
-                <td><input type="text"  className="form-control" style={{background:"rgb(241 98 45 / 56%)",fontWeight:"bold"}} name="" id="" /></td>
+                <td  style={Calculator['myPayablePrice'] == null? {display:""}:{display:"none"}}>
+                    <input type="text" disabled   value={ orderData.length != 0? orderData[0]['payable_price']:''}  className="form-control" style={{background:"rgb(241 98 45 / 56%)",fontWeight:"bold"}} name="" id="" />
+                    
+                    </td>
+
+                    <td  style={Calculator['myPayablePrice'] == null? {display:"none"}:{display:""}}>
+                    
+                    <input type="text" disabled   value={ Calculator['myPayablePrice'] }  className="form-control" style={{background:"rgb(241 98 45 / 56%)",fontWeight:"bold"}} name="" id="" />
+                    
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Pkaard Charge </td>
+                    <td style={{fontSize:"11px"}}> Charge <b>{Calculator['Charge']}</b> tk VAT(15%) <b>{Calculator['Vat']}</b> tk Grand Total <b>{Calculator['grandTotal']}</b>tk  </td>
                 </tr>
 
                 <tr>
--- src/app/User/new_order/page.jsx
+++ src/app/User/new_order/page.jsx
@@ -3,31 +3,23 @@ import { useState,useEffect } from "react";
 import { useSelector } from "react-redux";
 import http from "../../../../Component/http";
 import Link from "next/link";
-
+import { useRouter } from 'next/navigation'
   function  NewOrder() {
   const [OrderData, setOrderData]= useState([]);
 
   const {user_data} = useSelector((state)=>state.loginAction);
  const {id} =user_data[0];
     useEffect( ()=>{
-        getData();
+       getData();
     },[])
- 
-    
+  
+    const router = useRouter()
      function getData() {
-
-       
         console.log(user_data)
        
     
         // console.log(id)
-    
-      
-       
-
-       
-
-        http.get(`getOneOrder/${id}`)
+  http.get(`order_card_holder/${id}`)
         .then(data=>{
             if(data.status==200){
                 setOrderData(data.data)
@@ -37,6 +29,24 @@ import Link from "next/link";
         })
        
       }
+
+
+
+      let i = 1;
+   let OrderView =    OrderData.map((d)=>{
+                   
+        return(<>
+         <tr key={i+1}>
+         <td>{i++}</td>
+         <td>{d['discount_promo_code']}</td>
+         <td>{d['full_name']}</td>
+         <td>
+         <button  onClick={() => router.push(`User/get_bill?order_id=${d['id']}`)} className="btn btn-info btn-sm btn-block">Get Bill </button> 
+     
+         </td>
+     </tr>
+     </>)
+    })
 
     
     return ( <>
@@ -52,34 +62,15 @@ import Link from "next/link";
             <thead>
             <tr>
                 <th>#</th>
-                <th>Card No </th>
+                <th>Coupon no</th>
                 <th>Name </th>
-                <th>Mobile No</th>
-                <th>Payable amount</th>
+                       
                 <th>Action </th>
             </tr>
             </thead>
 
             <tbody>
-                {OrderData.map((d)=>{
-                    let i = 1;
-                    return(<>
-                     <tr key={i}>
-                     <td>{i++}</td>
-                     <td>1509002{d['card_registation_card_id']}</td>
-                     <td>{d['contact_full_name']}</td>
-                    
-                     <td>01890492444</td>
-                     <td>450 TK</td>
-                     <td>
-
-                     <Link href="User/get_bill" className="btn btn-info btn-sm btn-block">Get Bill </Link> 
-                 
-                     </td>
-                 </tr>
-                 </>)
-                })}
-           
+              {OrderView}
         
             </tbody>
         </table>
